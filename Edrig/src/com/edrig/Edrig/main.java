0f4bf16513bc9dd3a605b018f1bb8a02a68f68ac
Edrig <edrig@edrig7720>
2014-01-27 23:19:13 +0100

    Committer: Edrig <edrig@edrig7720>
     On branch master
     Changes to be committed:
    	modified:   Edrig/bin/com/edrig/Edrig/main.class
    	modified:   Edrig/plugin.yml
    	modified:   Edrig/src/com/edrig/Edrig/main.java
    	modified:   README.md
    
    first test scoreboard

--- Edrig/src/com/edrig/Edrig/main.java
+++ Edrig/src/com/edrig/Edrig/main.java
@@ -2,39 +2,69 @@ package com.edrig.Edrig;
 
 import java.util.logging.Logger;
 
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scoreboard.DisplaySlot;
+import org.bukkit.scoreboard.Objective;
+import org.bukkit.scoreboard.Scoreboard;
+import org.bukkit.scoreboard.ScoreboardManager;
 
 public class main extends JavaPlugin implements Listener
 {
     public final Logger logger = Logger.getLogger("Minecraft");
-   
-    @Override
+    
+    ScoreboardManager manager = Bukkit.getScoreboardManager();
+    Scoreboard board = manager.getNewScoreboard();
+    Objective objective = board.registerNewObjective("HP", "health");
+    
+    
+	@Override
     public void onEnable() 
     {
+		Bukkit.getServer().getPluginManager().registerEvents(this, this);
         getCommand("yolo").setExecutor(new EdrigCommandExecutor(this));
-    }
 
-	@Override
-    public void onDisable() 
-	{
-		
-    }
+
+        objective.setDisplaySlot(DisplaySlot.SIDEBAR);
+        objective.setDisplayName("HP");
+        
        
-
-
-    @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent event) 
-    {
-    	
+        
+        /*for(Player online : Bukkit.getOnlinePlayers()){
+          Score score = objective.getScore(online);
+          score.setScore(5); //Example
+        }
+         
+        for(Player online : Bukkit.getOnlinePlayers()){
+         online.setScoreboard(board);
+        }*/
     }
-    
-    @EventHandler
-    public void onPlayerQuit(PlayerQuitEvent event) 
-    {
 
-    }
+	public void onDisable()
+	{
+        /*for(Player online : Bukkit.getOnlinePlayers()){
+        	online.setScoreboard(manager.getNewScoreboard());
+        }*/
+	}
+	
+	@EventHandler
+	public void onPlayerJoin(PlayerJoinEvent evt)
+	{
+	Player player = evt.getPlayer();
+	player.setScoreboard(board);
+    /*Score score = objective.getScore(player);
+    score.setScore(5); //Example*/
+	}
+	
+	@EventHandler
+	 public void onPlayerQuit(PlayerQuitEvent evt)
+	{
+	Player player = evt.getPlayer();
+	board.resetScores(player);
+	}
 }
