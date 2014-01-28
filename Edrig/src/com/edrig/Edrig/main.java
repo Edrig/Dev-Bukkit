8db6aafd2e0a53ea0ad3f2003970eafabc41aee4
Edrig <gaetan.noel@viacesi.fr>
2014-01-28 11:47:41 +0100

    erreur initialisation scoreboard

--- Edrig/src/com/edrig/Edrig/main.java
+++ Edrig/src/com/edrig/Edrig/main.java
@@ -17,32 +17,22 @@ import org.bukkit.scoreboard.ScoreboardManager;
 public class main extends JavaPlugin implements Listener
 {
     public final Logger logger = Logger.getLogger("Minecraft");
+	private Scoreboard board;
+    //Scoreboard board;
     
-    ScoreboardManager manager = Bukkit.getScoreboardManager();
-    Scoreboard board = manager.getNewScoreboard();
-    Objective objective = board.registerNewObjective("HP", "health");
-    
-    
-	@Override
-    public void onEnable() 
+	public void onEnable() 
     {
 		Bukkit.getServer().getPluginManager().registerEvents(this, this);
         getCommand("yolo").setExecutor(new EdrigCommandExecutor(this));
-
-
+        
+        ScoreboardManager manager = Bukkit.getScoreboardManager();
+        Scoreboard board = manager.getNewScoreboard();
+        Objective objective = board.registerNewObjective("HP", "health");
+        
         objective.setDisplaySlot(DisplaySlot.SIDEBAR);
         objective.setDisplayName("HP");
         
-       
-        
-        /*for(Player online : Bukkit.getOnlinePlayers()){
-          Score score = objective.getScore(online);
-          score.setScore(5); //Example
-        }
-         
-        for(Player online : Bukkit.getOnlinePlayers()){
-         online.setScoreboard(board);
-        }*/
+        this.board = board;
     }
 
 	public void onDisable()
@@ -67,4 +57,5 @@ public class main extends JavaPlugin implements Listener
 	Player player = evt.getPlayer();
 	board.resetScores(player);
 	}
+
 }
